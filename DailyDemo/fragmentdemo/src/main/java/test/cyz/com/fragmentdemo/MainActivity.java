c660a1ed6471343a8b1f00d0a5bc4cf3c8ca462a
jackmans <756349240@q.com>
2016-09-09 19:53:32 +0800

    新闻客户端demo，在写...

--- DailyDemo/fragmentdemo/src/main/java/test/cyz/com/fragmentdemo/MainActivity.java
+++ DailyDemo/fragmentdemo/src/main/java/test/cyz/com/fragmentdemo/MainActivity.java
@@ -2,15 +2,32 @@ package test.cyz.com.fragmentdemo;
 
 import android.graphics.Color;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import org.w3c.dom.Text;
+import org.w3c.dom.ls.LSInput;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import test.cyz.com.fragmentdemo.model.Category;
+
+public class MainActivity extends FragmentActivity implements View.OnClickListener{
 
     private TextView msgText;
     private TextView newText;
@@ -23,16 +40,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Fragment newsFrag;
     private Fragment settingFrag;
 
+
+//    private ListView listView;
+//    private TextView textView;
+//    private LinearLayout categoryLinearLayout;
+//    private LayoutInflater layoutInflater;
+//    private GridView gridView;
+//    private List<Category> categoryList = new ArrayList<Category>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+//        initNewsCategory();
         fragmentManager = getSupportFragmentManager();
         setSelection(0);
 
-
+//        categoryLinearLayout =(LinearLayout) findViewById(R.id.title_category);
+//        layoutInflater = LayoutInflater.from(this);
+//
+//        categoryLinearLayout.addView(layoutInflater.inflate(R.layout.layout_grid_category, null));
+//        gridView = (GridView)findViewById(R.id.gridView);
+//        initGridView();
+//        gridViewAdapter = new GirdViewAdapter(this, R.layout.ly_category_item, categoryList);
+//        gridView.setAdapter(gridViewAdapter);
+//        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Log.d("news", "选中了第" + i);
+//                TextView t;
+//                for(int a = 0; a < adapterView.getCount(); a++)
+//                {
+//                    t = (TextView) adapterView.getChildAt(a);
+//                    t.setTextColor(Color.BLACK);
+//                }
+//                t = (TextView) view;
+//                t.setTextColor(Color.RED);
+//            }
+//        });
     }
+
+//    private void initGridView() {
+//        int size = categoryList.size();
+//        int length = 60;
+//        DisplayMetrics dm = new DisplayMetrics();
+//        getWindowManager().getDefaultDisplay().getMetrics(dm);
+//        float density = dm.density;
+//        int gridviewWidth = (int) (size * (length + 4) * density);
+//        int itemWidth = (int)(length * density);
+//
+//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(gridviewWidth, LinearLayout.LayoutParams.MATCH_PARENT);
+//        gridView.setLayoutParams(params);
+//        gridView.setColumnWidth(itemWidth);
+//        gridView.setHorizontalSpacing(3);
+//        gridView.setStretchMode(GridView.NO_STRETCH);
+//        gridView.setNumColumns(size);
+//    }
 
     private void initView(){
         msgText = (TextView)findViewById(R.id.tv_msg);
@@ -43,8 +107,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         newText.setOnClickListener(this);
         contactText.setOnClickListener(this);
         settingText.setOnClickListener(this);
-
-
     }
 
     @Override
